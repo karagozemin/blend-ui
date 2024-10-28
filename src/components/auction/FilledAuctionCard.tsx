7d047bf906294a8d4c874f5164248a580a432b00
Ryang-21 <ryanyang2001@gmail.com>
2024-10-28 16:22:01 -0600

    remove auction profit display for FillAuctionCard component

--- src/components/auction/FilledAuctionCard.tsx
+++ src/components/auction/FilledAuctionCard.tsx
@@ -1,9 +1,6 @@
 import { AuctionType, Pool, ScaledAuction } from '@blend-capital/blend-sdk';
 import { Box, Typography, useTheme } from '@mui/material';
-import { useMemo } from 'react';
-import { useBackstop, useHorizonAccount, usePoolOracle, useTokenBalance } from '../../hooks/api';
-import { calculateAuctionOracleProfit } from '../../utils/auction';
-import { toBalance, toCompactAddress } from '../../utils/formatter';
+import { toCompactAddress } from '../../utils/formatter';
 import { DividerSection } from '../common/DividerSection';
 import { PoolComponentProps } from '../common/PoolComponentProps';
 import { Row } from '../common/Row';
@@ -20,47 +17,7 @@ export interface FilledAuctionCardProps extends PoolComponentProps {
 
 export const FilledAuctionCard: React.FC<FilledAuctionCardProps> = ({ pool, auction, sx }) => {
   const theme = useTheme();
-  const { data: poolOracle } = usePoolOracle(pool);
-  const { data: backstop } = useBackstop();
-  const { data: horizonAccount } = useHorizonAccount();
 
-  const assetKeys = useMemo(() => {
-    if (auction.type === AuctionType.Interest) {
-      const assets = new Set(auction.data.lot.keys());
-      for (const asset of Array.from(auction.data.bid.keys())) {
-        assets.add(asset);
-      }
-      return Array.from(assets);
-    }
-    return [];
-  }, [auction]);
-
-  const assetBalances = new Map<string, bigint>();
-  assetKeys.forEach((asset) =>
-    assetBalances.set(
-      asset,
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      useTokenBalance(asset, pool.reserves.get(asset)?.tokenMetadata.asset, horizonAccount).data ??
-        BigInt(0)
-    )
-  );
-
-  const auctionValue = useMemo(() => {
-    if (poolOracle && backstop) {
-      try {
-        return calculateAuctionOracleProfit(
-          auction.data,
-          auction.type,
-          pool,
-          poolOracle,
-          backstop.backstopToken
-        );
-      } catch (e) {
-        console.error('Error calculating auction value', e);
-      }
-    }
-    return undefined;
-  }, [auction, pool, poolOracle, backstop]);
   return (
     <Section width={SectionSize.FULL} sx={{ flexDirection: 'column', marginBottom: '12px', ...sx }}>
       <Box
@@ -107,15 +64,6 @@ export const FilledAuctionCard: React.FC<FilledAuctionCardProps> = ({ pool, auct
             sx={{ width: '50%' }}
           ></StackedTextBox>
         </Row>
-        {auctionValue !== undefined && (
-          <Row>
-            <StackedTextBox
-              name="Estimated Oracle Profit"
-              text={`${toBalance(auctionValue.lot - auctionValue.bid, 3)}`}
-              sx={{ width: '100%' }}
-            ></StackedTextBox>
-          </Row>
-        )}
       </Box>
       <LotList
         pool={pool}
