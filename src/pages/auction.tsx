c64f406664677411375eb9f7de2cbad8586ecbc9
llama <llama@llama.fi>
2024-11-06 13:29:57 -0800

    fix querying empty poolIds, display loading skeleton on pool and event fetching, and make banner messages consistent

--- src/pages/auction.tsx
+++ src/pages/auction.tsx
@@ -1,11 +1,11 @@
-import { Auctions, getAuctionsfromEvents } from '@blend-capital/blend-sdk';
-import { Typography, useTheme } from '@mui/material';
+import { getAuctionsfromEvents } from '@blend-capital/blend-sdk';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import { Box, Typography, useTheme } from '@mui/material';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { FilledAuctionCard } from '../components/auction/FilledAuctionCard';
 import { OngoingAuctionCard } from '../components/auction/OngoingAuctionCard';
-import { Banner } from '../components/common/Banner';
 import { Divider } from '../components/common/Divider';
 import { Row } from '../components/common/Row';
 import { Skeleton } from '../components/common/Skeleton';
@@ -16,7 +16,6 @@ import {
   useAuctionEventsShortQuery,
   useBackstop,
   usePool,
-  usePoolOracle,
 } from '../hooks/api';
 
 const Auction: NextPage = () => {
@@ -25,19 +24,29 @@ const Auction: NextPage = () => {
   const { poolId } = router.query;
   const safePoolId = typeof poolId == 'string' && /^[0-9A-Z]{56}$/.test(poolId) ? poolId : '';
   const { txStatus } = useWallet();
-  const { data: pool } = usePool(safePoolId);
-  const { data: oracle } = usePoolOracle(pool);
+  const {
+    data: pool,
+    isFetched: isPoolFetched,
+    isError: isPoolLoadingError,
+  } = usePool(safePoolId, safePoolId !== '');
   const { data: backstop } = useBackstop();
-  let { data: events, refetch } = useAuctionEventsLongQuery(safePoolId);
-  let curser =
-    Array.isArray(events) && events[events.length - 1] ? events[events.length - 1].id : '';
-  const { data: recentEvents } = useAuctionEventsShortQuery(safePoolId, curser, curser !== '');
+  let {
+    data: events,
+    refetch,
+    isFetched: isLongEventsFetched,
+  } = useAuctionEventsLongQuery(safePoolId, safePoolId !== '');
+  let curser = Array.isArray(events) && events.length > 0 ? events[events.length - 1].id : '';
+  const moreEventsToFetch = curser !== '';
+  const { data: recentEvents, isFetched: isShortEventsFetched } = useAuctionEventsShortQuery(
+    safePoolId,
+    curser,
+    moreEventsToFetch && safePoolId !== ''
+  );
   events = events?.concat(recentEvents?.events ?? []);
-  const auctions: Auctions =
-    oracle && pool && backstop && events && recentEvents
+  const auctions =
+    pool && backstop && events && events.length > 0
       ? getAuctionsfromEvents(events, backstop.id)
-      : { ongoing: [], filled: [] };
-
+      : undefined;
   useEffect(() => {
     if (txStatus === TxStatus.SUCCESS) {
       refetch();
@@ -50,42 +59,71 @@ const Auction: NextPage = () => {
       </Row>
       <Divider />
 
-      {pool !== undefined ? (
-        <>
-          {auctions.ongoing.map((auction, index) => {
-            return (
-              <OngoingAuctionCard
-                key={index}
-                index={index}
-                auction={auction}
-                poolId={safePoolId}
-                pool={pool}
-                currLedger={recentEvents?.latestLedger ? recentEvents.latestLedger : 0}
-              />
-            );
-          })}
-          {auctions.filled.reverse().map((auction, index) => {
-            return (
-              <FilledAuctionCard
-                key={index}
-                index={index}
-                auction={auction}
-                poolId={safePoolId}
-                pool={pool}
-              />
-            );
-          })}
-          {auctions.ongoing.length === 0 && auctions.filled.length === 0 && (
-            <Banner
+      {isPoolFetched && isLongEventsFetched && (isShortEventsFetched || !moreEventsToFetch) ? (
+        !isPoolLoadingError && pool ? (
+          auctions ? (
+            <>
+              {auctions.ongoing.map((auction, index) => {
+                return (
+                  <OngoingAuctionCard
+                    key={index}
+                    index={index}
+                    auction={auction}
+                    poolId={safePoolId}
+                    pool={pool}
+                    currLedger={recentEvents?.latestLedger ? recentEvents.latestLedger : 0}
+                  />
+                );
+              })}
+              {auctions.filled.reverse().map((auction, index) => {
+                return (
+                  <FilledAuctionCard
+                    key={index}
+                    index={index}
+                    auction={auction}
+                    poolId={safePoolId}
+                    pool={pool}
+                  />
+                );
+              })}
+            </>
+          ) : (
+            <Row
               sx={{
                 color: theme.palette.grey[100],
                 backgroundColor: theme.palette.grey[900],
+                display: 'flex',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                margin: '6px',
+                padding: '12px',
+                paddingRight: '20px',
+                borderRadius: '5px',
               }}
             >
               <Typography variant="body2">{'No ongoing auctions for this pool'}</Typography>
-            </Banner>
-          )}
-        </>
+            </Row>
+          )
+        ) : (
+          <Row
+            sx={{
+              background: theme.palette.warning.opaque,
+              color: theme.palette.warning.main,
+              display: 'flex',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+              margin: '6px',
+              padding: '12px',
+              paddingRight: '20px',
+              borderRadius: '5px',
+            }}
+          >
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+              <InfoOutlinedIcon sx={{ marginRight: '6px' }} />
+              <Typography variant="body2">Unable to load pool data.</Typography>
+            </Box>
+          </Row>
+        )
       ) : (
         <Skeleton />
       )}
