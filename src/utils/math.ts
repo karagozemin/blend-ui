f9054208edac7d6b97d12e21a5e41f8e892e48c4
Ryang-21 <Ryang-21@users.noreply.github.com>
2024-11-11 15:45:59 -0700

    add interest rate estimation helper

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,4 +1,4 @@
-import { BackstopToken, FixedMath } from '@blend-capital/blend-sdk';
+import { BackstopToken, FixedMath, Reserve } from '@blend-capital/blend-sdk';
 
 /**
  * Estimate the emissions apr for a reserve
@@ -16,4 +16,33 @@ export function estimateEmissionsApr(
     0.2 /
     (FixedMath.toFloat(backstopToken.blnd, 7) / 0.8);
   return (emissionsPerAssetPerYear * usdcPerBlnd) / assetPrice;
+}
+
+/**
+ * Estimate the interest rate for a reserve given a utilization ratio
+ * @param utilizationRatio utilization ratio as a float
+ * @param reserve The reserve to estimate the interest rate for
+ */
+export function estimateInterestRate(utilizationRatio: number, reserve: Reserve): number {
+  let rateModifier = FixedMath.toFloat(reserve.data.interestRateModifier, 9);
+  let baseRate = reserve.config.r_base / 1e7;
+  let targetUtilization = reserve.config.util / 1e7;
+  let interestRate = 0;
+  const rateOne = reserve.config.r_one / 1e7;
+  const rateTwo = reserve.config.r_two / 1e7;
+  const rateThree = reserve.config.r_three / 1e7;
+  if (utilizationRatio <= targetUtilization) {
+    interestRate = rateModifier * (baseRate + (utilizationRatio / targetUtilization) * rateOne);
+  } else if (utilizationRatio > targetUtilization && utilizationRatio <= 0.95) {
+    interestRate =
+      rateModifier *
+      (baseRate +
+        rateOne +
+        ((utilizationRatio - targetUtilization) / (0.95 - targetUtilization)) * rateTwo);
+  } else if (utilizationRatio > 0.95) {
+    interestRate =
+      rateModifier * (baseRate + rateOne + rateTwo) +
+      ((utilizationRatio - 0.95) / 0.05) * rateThree;
+  }
+  return interestRate;
 }
